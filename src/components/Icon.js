3fca3216ffdf99b8aa49c97dd75071357b596d68
Paramjit singh <paramjeet.flymediatech@gmail.com>
2026-04-17 11:56:44 +0530

    fix component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,10 +1,10 @@
 import * as icons from 'ionicons/icons'
 
-
-const toCamelCase = (str) =>
-    str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase())
-
 export default function Icon({ name }) {
+    const toCamelCase = (str) => {
+        if (!str) return '';
+        return str.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase())
+    }
     const iconKey = toCamelCase(name)
     const icon = icons[iconKey] || icons.helpCircleOutline
     return <ion-icon icon={icon}></ion-icon>
